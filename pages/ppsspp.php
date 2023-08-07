bfb1139f1f36dd2ff2a240142cf116b8adeb5e7b
Barez0 <barezgardi0@gmail.com>
2023-08-07 16:50:21 +0300

    b

--- pages/ppsspp.php
+++ pages/ppsspp.php
@@ -68,7 +68,7 @@
         Download App
       </div>
                            <div class="item-title-row block block-strong inset elevation-12" style="justify-content: flex-end;">
-                            <a href="https://api.nekoo.ru/view/BDfMmJLm" class="button button-round button-fill button-raised color-green external"> ㅤGet <i class="icon f7-icons color-white">cloud_download_fill</i></a>
+                                                                                    <a href="itms-services://?action=download-manifest&amp;url=https://barezstore.netlify.app/plist/ppsspp.plist" class="button button-round button-fill button-raised color-green external"> ㅤGet <i class="icon f7-icons color-white">cloud_download_fill</i></a>
 <br>
                              
                  <a href="" class="button button-round button-fill button-raised color-lightblue external">IPA   <i class="icon f7-icons color-white">cloud_download_fill</i></a>
